ba1f0e6d450a71c08c5dfc52dbe830f9546b3a12
Rohaan Mansoor <rohaanmansoor226@gmail.com>
2026-08-03 21:11:10 -0400

    chore: locked the cursor in play mode by default for a smoother testing and gameplay experience. Can be toggled via Escape key.

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -9,13 +9,20 @@ public class PlayerInputHandler : MonoBehaviour
     
     public bool JumpPressed { get; private set; }
     public bool SprintActive { get; private set; }
+    
     public bool FireHeld { get; private set; }
     public bool ReloadPressed { get; private set; }
     public bool InteractActive { get; private set; }
-
+    
     private void Awake()
     {
         _inputActions = new PlayerInputActions();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     
     private void OnEnable()
@@ -31,6 +38,8 @@ public class PlayerInputHandler : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        LockCursor();
+        
         MoveInput = _inputActions.Player.Move.ReadValue<Vector2>();
         LookInput = _inputActions.Player.Look.ReadValue<Vector2>();
         
@@ -39,5 +48,18 @@ public class PlayerInputHandler : MonoBehaviour
         
         FireHeld = _inputActions.Player.Fire.WasPressedThisFrame();
         ReloadPressed = _inputActions.Player.Reload.WasPressedThisFrame();
+    }
+
+    private void LockCursor()
+    {
+        if (_inputActions.Player.ToggleCursor.WasPressedThisFrame())
+        {
+            Cursor.lockState =
+                (Cursor.lockState == CursorLockMode.Locked) ? 
+                    CursorLockMode.None : 
+                    CursorLockMode.Locked;
+
+            Cursor.visible = !Cursor.visible;
+        }
     }
 }
